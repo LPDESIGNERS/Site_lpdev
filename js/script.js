b7d14bbcb1b3bcfc254b50a231f0e2cffad26108
lpdesigners <lpdesigners2024@gmail.com>
2024-07-09 21:19:20 -0300

    beta0.3
    Novos produtos adicionados

--- js/script.js
+++ js/script.js
@@ -18,3 +18,23 @@ function filterProducts(category) {
         }
     });
 }
+function toggleSubmenu(event) {
+    event.preventDefault();
+    const submenuContent = event.target.nextElementSibling;
+    submenuContent.style.display = submenuContent.style.display === 'block' ? 'none' : 'block';
+}
+
+function filterProducts(category) {
+    const products = document.querySelectorAll('.product');
+    products.forEach(product => {
+        if (category === 'all') {
+            product.style.display = 'block';
+        } else {
+            if (product.classList.contains(category)) {
+                product.style.display = 'block';
+            } else {
+                product.style.display = 'none';
+            }
+        }
+    });
+}
