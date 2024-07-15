69df6195fdee63cfd7100a5a3445cd5b7cefd5b0
lpdesigners <lpdesigners2024@gmail.com>
2024-07-15 16:27:53 -0300

    Adaptando para smartphones

--- js/script.js
+++ js/script.js
@@ -38,3 +38,29 @@ function filterProducts(category) {
         }
     });
 }
+function toggleDropdown(event) {
+    event.preventDefault();
+    const dropdownContent = event.target.nextElementSibling;
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+}
+
+function toggleSubmenu(event) {
+    event.preventDefault();
+    const submenuContent = event.target.nextElementSibling;
+    submenuContent.style.display = submenuContent.style.display === 'block' ? 'none' : 'block';
+}
+
+function filterProducts(category) {
+    const products = document.querySelectorAll('.product');
+    products.forEach(product => {
+        if (category === 'all') {
+            product.style.display = 'block';
+        } else {
+            if (product.classList.contains(category)) {
+                product.style.display = 'block';
+            } else {
+                product.style.display = 'none';
+            }
+        }
+    });
+}
